d292ee82aaccadbe0d961e7c3c42bd5df50f4479
buidennis <dennistbui@gmail.com>
2021-11-06 11:47:07 -0700

    Search Component Wireframe

---
+++ book-with-hooks/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Search } from './components/Search';
+
+function App() {
+    return (
+        <header>
+            <Search />
+        </header>
+    );
+}
+
+export default App;
