12cb8d971ac4621c2a48823c84317de0e67919fe
buidennis <dennistbui@gmail.com>
2021-11-09 08:36:42 -0800

    Completed

--- book-with-hooks/src/App.js
+++ book-with-hooks/src/App.js
@@ -1,5 +1,44 @@
+import React from 'react';
 import './App.css';
 import { Search } from './components/Search';
+/**
+function Glossary(props) {
+    return (
+        <dl>
+            {props.items.map(item => (
+                <React.Fragment key={item.id}>
+                    <dt>{item.term}</dt>
+                    <dd>{item.description}</dd>
+                </React.Fragment>
+            ))}
+        </dl>
+    );
+}
+
+function Message(props) {
+    return (
+        <div className="message">
+            {props.children}
+        </div>
+    )
+}
+const element = (
+    <div className="container">
+        <Message>
+            Outer Layer
+            <Message> Inner Layer </Message>
+        </Message>
+    </div>
+)
+
+function SayHello({ firstName, lastName }) {
+    return (
+        <div>
+            Hello {firstName} {lastName}!
+        </div>
+    )
+};
+*/
 
 function App() {
     return (
--- book-with-hooks/src/components/Search.js
+++ book-with-hooks/src/components/Search.js
@@ -1,10 +1,42 @@
 import React from 'react';
 
+const baseUrl = 'http://openlibrary.org';
+
+export function searchBooks(query) {
+    const url = new URL(baseUrl + '/search.json');
+    url.searchParams.append('title', query);
+
+    return fetch(url).then(response => response.json());
+}
+
 export function Search() {
+    // useState, obtains the stateful variable
+    // return [currentValue of the state variable,
+    // function called to update the state]
+    // [currentState, stateUpdaterFunction()]
+    const [results, setResults] = React.useState(0);
+
+    // searchBooks, event handler 
+    // gets a response from the server and updates the state
+    const handleSearch = (event) => {
+        searchBooks(event.target.value).then(response => {
+            setResults(response.docs);
+        });
+    };
+    // resultsList, representation of search results in array
+    // of HTML table rows
+    const resultsList = (results || []).map((book) =>
+        <tr key={book.key}>
+            <td>{book.title}</td>
+            <td>{book.author_name && book.author_name.join(', ')}</td>
+            <td>{book.first_publish_year}</td>
+        </tr>
+    )
+
     return (
         <div>
             <div className="search-input">
-                <input type="text" placeholder="Search" />
+                <input type="text" placeholder="Search" onChange={handleSearch} />
             </div>
             <h1 className="h1">Search Results</h1>
             <div className="books">
@@ -16,7 +48,9 @@ export function Search() {
                             <th className="year-col">Pub Year</th>
                         </tr>
                     </thead>
-                    <tbody></tbody>
+                    <tbody>
+                        {resultsList}
+                    </tbody>
                 </table>
             </div>
         </div>
